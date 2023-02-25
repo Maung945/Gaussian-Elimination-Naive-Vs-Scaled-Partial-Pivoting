749843a0bd577c846acb4736574cd4ba0354f917
Maung945 <myothura85@gmail.com>
2023-02-24 17:47:57 -0800

    Added Test Results

--- extension.sol
+++ extension.sol
@@ -1,1 +1,41 @@
 
+Solution Naive Gaussian Algorithm: 
+    0.216025 -0.00791511    0.635243    0.746174
+Time Taken by Naive Gaussian Algorithm: 1-Microseconds
+
+Solution Scaled Partial Pivoting: 
+    0.216025 -0.00791511    0.635243    0.746174
+Time Taken by SPP Gaussian Algorithm: 4-Microseconds.
+
+Solution Naive Gaussian Algorithm: 
+    0.216025 -0.00791511    0.635243    0.746174
+Time Taken by Naive Gaussian Algorithm: 1-Microseconds
+
+Solution Naive Gaussian Algorithm: 
+    0.216025 -0.00791511    0.635243    0.746174
+Time Taken by Naive Gaussian Algorithm: 2-Microseconds
+
+Solution Naive Gaussian Algorithm: 
+    0.216025 -0.00791511    0.635243    0.746174
+Time Taken by Naive Gaussian Algorithm: 1-Microseconds
+
+Solution Scaled Partial Pivoting: 
+    0.216025 -0.00791511    0.635243    0.746174
+Time Taken by SPP Gaussian Algorithm: 4-Microseconds.
+
+Solution Scaled Partial Pivoting: 
+    0.216025 -0.00791511    0.635243    0.746174
+Time Taken by SPP Gaussian Algorithm: 4-Microseconds.
+
+Solution Scaled Partial Pivoting: 
+    0.216025 -0.00791511    0.635243    0.746174
+Time Taken by SPP Gaussian Algorithm: 10-Microseconds.
+
+Solution Scaled Partial Pivoting: 
+    0.216025 -0.00791511    0.635243    0.746174
+Time Taken by SPP Gaussian Algorithm: 6-Microseconds.
+
+Solution Naive Gaussian Algorithm: 
+    0.216025 -0.00791511    0.635243    0.746174
+Time Taken by Naive Gaussian Algorithm: 1-Microseconds
+
